d070050c8cf9b084bb9f6d6d416dc94e03e3f4ba
denis ptica <kotlinkod@gmail.com>
2025-03-18 12:40:38 +0400

    Add .gitignore file to exclude specified files

---
+++ build-logic/base/build/kotlin-dsl/plugins-blocks/extracted/kmp.compose.config.gradle.kts
@@ -0,0 +1,10 @@
+                      
+                                                       
+
+plugins {
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.compose")
+    id("kmp.base.config")
+                                  
+    id("android.base.config")
+}
