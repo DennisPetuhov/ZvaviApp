34cf1763b44187f1bc66a12b0a1fd27d4205edac
denis ptica <kotlinkod@gmail.com>
2025-03-18 17:03:24 +0400

    trying to merge in develop

---
+++ build-logic/base/src/main/kotlin/kmp.compose.config.gradle.kts
@@ -0,0 +1,62 @@
+import extensions.libs
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.compose")
+    id("kmp.base.config")
+    id("android.base.config")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.kermit)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.serialization)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.navigation.composee)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.coil)
+            implementation(libs.coil.network.ktor)
+            implementation(libs.multiplatformSettings)
+            implementation(libs.kotlinx.datetime)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+
+        androidMain.dependencies {
+            implementation(compose.uiTooling)
+            implementation(libs.androidx.activityCompose)
+            implementation(libs.kotlinx.coroutines.android)
+            implementation(libs.ktor.client.okhttp)
+        }
+
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
+            implementation(libs.ktor.client.okhttp)
+        }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+
+    }
+}
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,112 +1,26 @@
-import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 
 plugins {
     alias(libs.plugins.android.application)
     id("kmp.base.config")
-//    alias(libs.plugins.multiplatform)
-//    alias(libs.plugins.compose.compiler)
-//    alias(libs.plugins.compose)
-
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.buildConfig)
     id("kmp.compose.config")
-//    id("kmp.base.config")
 
 }
 kotlin {
     jvmToolchain(17)// this is for compiler
-//    androidTarget ()
-//    androidTarget {
-//        //https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-test.html
-//        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-//    }
-//    jvm()
-//    listOf(
-//        iosX64(),
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach {
-//        it.binaries.framework {
-//            baseName = "ComposeApp"
-//            isStatic = true
-//        }
-//    }
-
-    sourceSets {
-//        commonMain.dependencies {
-//            implementation(compose.runtime)
-//            implementation(compose.foundation)
-//            implementation(compose.material3)
-//            implementation(compose.components.resources)
-//            implementation(compose.components.uiToolingPreview)
-//            implementation(libs.kermit)
-//            implementation(libs.kotlinx.coroutines.core)
-//            implementation(libs.ktor.client.core)
-//            implementation(libs.ktor.client.content.negotiation)
-//            implementation(libs.ktor.client.serialization)
-//            implementation(libs.ktor.client.logging)
-//            implementation(libs.androidx.lifecycle.viewmodel)
-//            implementation(libs.androidx.lifecycle.runtime.compose)
-//            implementation(libs.androidx.navigation.composee)
-//            implementation(libs.kotlinx.serialization.json)
-//            implementation(libs.koin.core)
-//            implementation(libs.koin.compose)
-//            implementation(libs.coil)
-//            implementation(libs.coil.network.ktor)
-//            implementation(libs.multiplatformSettings)
-//            implementation(libs.kotlinx.datetime)
-//        }
-//
-//        commonTest.dependencies {
-//            implementation(kotlin("test"))
-//            @OptIn(ExperimentalComposeLibrary::class)
-//            implementation(compose.uiTest)
-//            implementation(libs.kotlinx.coroutines.test)
-//        }
-//
-//        androidMain.dependencies {
-//            implementation(compose.uiTooling)
-//            implementation(libs.androidx.activityCompose)
-//            implementation(libs.kotlinx.coroutines.android)
-//            implementation(libs.ktor.client.okhttp)
-//        }
-//
-//        jvmMain.dependencies {
-//            implementation(compose.desktop.currentOs)
-//            implementation(libs.kotlinx.coroutines.swing)
-//            implementation(libs.ktor.client.okhttp)
-//        }
-//
-//        iosMain.dependencies {
-//            implementation(libs.ktor.client.darwin)
-//        }
-//
-    }
+    sourceSets { }
 }
 
 android {
     namespace = "ge.avalanche.zvavi"
-//    compileSdk = 35
 
     defaultConfig {
-//        minSdk = 21
-//        targetSdk = 35
-
-//        applicationId = "ge.avalanche.zvavi.androidApp"
-//        versionCode = 1
-//        versionName = "1.0.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
 
-////https://developer.android.com/develop/ui/compose/testing#setup
-//dependencies {
-//    androidTestImplementation(libs.androidx.uitest.junit4)
-//    debugImplementation(libs.androidx.uitest.testManifest)
-//}
 
 compose.desktop {
     application {
@@ -130,8 +44,3 @@ compose.desktop {
         }
     }
 }
-
-//buildConfig {
-    // BuildConfig configuration here.
-    // https://github.com/gmazzo/gradle-buildconfig-plugin#usage-in-kts
-//}
