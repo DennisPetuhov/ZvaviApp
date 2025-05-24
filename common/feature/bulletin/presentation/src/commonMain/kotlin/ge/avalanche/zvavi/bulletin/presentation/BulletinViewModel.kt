01361aeaf0c246689e06473d12ce336d3953030f
Den Bird <kotlinkod@gmail.com>
2025-05-24 13:50:28 +0400

    blabla

--- common/feature/bulletin/presentation/src/commonMain/kotlin/ge/avalanche/zvavi/bulletin/presentation/BulletinViewModel.kt
+++ common/feature/bulletin/presentation/src/commonMain/kotlin/ge/avalanche/zvavi/bulletin/presentation/BulletinViewModel.kt
@@ -21,6 +21,7 @@ internal class BulletinViewModel(
     private val observeBulletinUseCase: ObserveBulletinUseCase,
     private val fetchBulletinUseCase: FetchBulletinUseCase,
     private val dispatchers: DispatchersProvider,
+
 ) : BaseViewModel<BulletinViewState, BulletinAction, BulletinEvent>(BulletinViewState.EMPTY) {
     private val logger = Logger.withTag("BulletinViewModel")
     private var bulletinJob: Job? = null
