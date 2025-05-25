7f103be44af90b883dce3b6109f1215a6c2b2b7f
Den Bird <kotlinkod@gmail.com>
2025-05-25 17:20:29 +0400

    blabla

--- common/feature/bulletin/presentation/src/commonMain/kotlin/ge/avalanche/zvavi/bulletin/presentation/BulletinViewModel.kt
+++ common/feature/bulletin/presentation/src/commonMain/kotlin/ge/avalanche/zvavi/bulletin/presentation/BulletinViewModel.kt
@@ -50,10 +50,6 @@ internal class BulletinViewModel(
     }
 
     fun handleReturnFromBulletinProblemScreen() {
-
-    }
-
-    fun handleReturnFromBulletinProblemScreen() {
         viewState = viewState.copy(showBottomSheet = true)
     }
 
